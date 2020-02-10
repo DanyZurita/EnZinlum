e7a39c1327c77d4581e7ed3bcac4d05e2ff7851a
DanyZurita <dzurita@cifpfbmoll.eu>
2020-02-10 20:59:01 +0100

    feat(KeyPair): Añadir KeyPairs y toString

--- src/main/java/enzinium/Address.java
+++ src/main/java/enzinium/Address.java
@@ -1,6 +1,42 @@
 package enzinium;
 
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
 
 public class Address {
+    private PublicKey PK;
+    private PrivateKey SK;
+    protected Double balance = 0.0;
+    public String symbol = "EZI";
+    public GenSig sig;
+         
     
+    public Address() {
+    
+    }
+    
+    public void generateKeyPair() {
+        KeyPair keys = GenSig.generateKeyPair();  
+        PK = keys.getPublic();
+        SK = keys.getPrivate();
+        
+    }
+    
+    @Override
+    public String toString(){
+        StringBuilder addressDescription = new StringBuilder();
+        addressDescription.append("PK =").append(PK.hashCode()).append('\n');
+        addressDescription.append("Balacne = ").append(balance).append(symbol);
+        return addressDescription.toString();
+    }
+    
+    public void addEZI(Double EZI) {
+        balance += EZI;
+    }
+    
+    public void transferEZI(Double EZI) {
+        
+    }
 }
--- src/test/java/enzinium/AddressTest.java
+++ src/test/java/enzinium/AddressTest.java
@@ -1,6 +1,14 @@
 package enzinium;
 
+import org.junit.Test;
+
 
 public class AddressTest {
     
+    @Test
+    public void createAddress(){
+        Address morty = new Address();
+        morty.generateKeyPair();
+        System.out.println(morty.toString()); 
+    }
 }
